b69df373903f221375b72d725c9dce6cbd90d19f
Hattorius <54858358+Hattorius@users.noreply.github.com>
2020-11-22 16:46:34 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,26 +15,27 @@ function loadPage(page) { // Put page content into the body
     var pageHolder = document.querySelector("body div#content");
     document.querySelector("body").id = page;
     pageHolder.innerHTML = pagesContent[page];
+    document.body.id = page;
 }
 
 function pageLoaded(page) {
     // Do additional stuff for each page to make buttons work etc.
     switch (page) {
         case 'home':
-            document.querySelector("#content > button").onclick = function() {
+            document.querySelector("body#home #content > button").onclick = function() {
                 goTo('more-info');
             }
             break;
         case 'more-info':
-            document.querySelector("#content > button:nth-child(1)").onclick = function() {
+            document.querySelector("body#more-info #content > button:nth-child(1)").onclick = function() {
                 var win = window.open('https://github.com/Hattorius/simple-javascript-routing', '_blank');
                 win.focus();
             }
-            document.querySelector("#content > button:nth-child(3)").onclick = function() {
+            document.querySelector("body#more-info #content > button:nth-child(3)").onclick = function() {
                 goTo('home');
             }
         case '404':
-            document.querySelector("#content > button").onclick = function() {
+            document.querySelector("body#404 #content > button").onclick = function() {
                 goTo('home');
             }
     }
